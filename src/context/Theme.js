ac5fe917d9ee09cae4b150ec1cbffc8ce44cfc7b
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-04 22:37:52 -0500

    ThemeContext createContext export .Provider .Consumer

---
+++ src/context/Theme.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const ThemeContext = createContext();
+
+export default ThemeContext;
+export const ThemeConsumer = ThemeContext.Consumer;
+export const ThemeProvider = ThemeContext.Provider;
