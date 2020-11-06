b5e487f268c2d64f78b34ef1b74dd6eb52ace5e6
Moises Sanabria <sesmoi.data@gmail.com>
2020-11-06 18:13:57 -0500

    reconfiguring webpack will downgrade to v4

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ["babel-loader"] },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
